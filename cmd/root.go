442ec125f88f1ac9e67e2066f2e5af39de7402a1
wujunwei <1399952803@qq.com>
2022-08-14 00:10:46 +0800

    支持定义配置文件

--- cmd/root.go
+++ cmd/root.go
@@ -1,23 +1,24 @@
 package cmd
 
 import (
+	"fmt"
 	"os"
+
+	"github.com/mitchellh/go-homedir"
+	"github.com/spf13/viper"
 
 	"github.com/spf13/cobra"
 )
 
-
+var (
+	cfgFile string // 自定义配置路径, 类似 ~/.bashrc
+)
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "bd",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "研发工具箱",
+	Long:  `研发工具箱`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
@@ -33,15 +34,32 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.bd.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// 加载默认配置
+	cobra.OnInitialize(initConfig)
+	// 指定自定义配置文件
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.bd.yaml)")
 }
 
+func initConfig() {
+	// Don't forget to read config either from cfgFile or from home directory!
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := homedir.Dir()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
 
+		// Search config in home directory with name ".bd.yaml"
+		viper.AddConfigPath(home)
+		viper.SetConfigName(".bd")
+		viper.SetConfigType("yaml")
+	}
+
+	if err := viper.ReadInConfig(); err != nil {
+		os.Exit(1)
+	}
+}
